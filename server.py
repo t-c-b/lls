13e653d40ae38d000e9877bc3da5ed8f8ed46865
tcb <tcb5qq@virginia.edu>
2023-10-03 23:38:42 -0400

    VLLM Branch

--- server.py
+++ server.py
@@ -14,22 +14,17 @@ from lsprotocol.types import (
 )
 
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
 from sys import argv
+
+from vllm import LLM
 
 logging.basicConfig(filename='log', filemode='w', level=logging.DEBUG)
 
-src = "TheBloke/CodeLlama-7B-Instruct-GPTQ"
-
 server = LanguageServer("example-server", "v0.1")
-model = AutoModelForCausalLM.from_pretrained(
-        src, 
-        device_map="auto",
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-        revision="gptq-4bit-32g-actorder_True"
-)
-tokenizer = AutoTokenizer.from_pretrained(src, use_fast=True)
+
+llm = LLM(model="codellama/CodeLlama-7b-hf", dtype='float16')
+llm_params = SamplingParams(temperature=0.3, top_p=0.95)
 
 @server.feature(TEXT_DOCUMENT_COMPLETION)
 def completions(params: CompletionParams):
@@ -44,35 +39,14 @@ def completions(params: CompletionParams):
     else:
         prefix = doc.source[:params.position.character]
         suffix = doc.source[params.position.character:]
-        #prefix = ''.join(doc.lines[:params.position.line])
-        #suffix = ''.join(doc.lines[params.position.line:])
-
-        #prompt = f"<PRE> {prefix} <SUF> {suffix} <MID>"
         prompt = prefix + "<FILL_ME>" + suffix
 
     server.show_message(message="Infilling...")
 
-    input_ids = tokenizer(prompt, return_tensors='pt').input_ids.cuda()
-    output = model.generate(
-        inputs=input_ids,
-        temperature=0.2,
-        do_sample=True,
-        top_p=0.9,
-        top_k=40,
-        max_new_tokens=512,
-        repetition_penalty=1.3,
-    )
-    #out = tokenizer.decode(output[0])
-    out = tokenizer.batch_decode(output[:, input_ids.shape[1]:], skip_special_tokens = True)[0]
-    #out = out[out.find("<MID>")+5 : out.find("<EOT>")]
-    logging.info(out)
-
-    lines = out.splitlines()
+    out = llm.generate([prompt], llm_params)[0].text
 
     item = CompletionItem(
             label = "llama",
-            #insert_text = out,
-            #insert_text_mode = InsertTextMode.AsIs,
             text_edit = TextEdit(
                 range = Range(start=pos, end=pos),
                 new_text = out,
@@ -82,3 +56,4 @@ def completions(params: CompletionParams):
     return CompletionList(is_incomplete=False, items=[item])
 
 server.start_io()
+server.show_message("Ready.")
