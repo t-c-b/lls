d3fc676c0deaca2f9739241cfb7705e6e0897d1c
tcb <tcb5qq@virginia.edu>
2023-10-07 15:25:41 -0400

    Code Cleanup

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ model = AutoModelForCausalLM.from_pretrained(
         device_map="auto",
         torch_dtype=torch.float16,
         trust_remote_code=True,
-        revision="gptq-4bit-32g-actorder_True"
+        revision="gptq-4bit-32g-actorder_True",
 )
 tokenizer = AutoTokenizer.from_pretrained(src, use_fast=True)
 
@@ -35,48 +35,49 @@ tokenizer = AutoTokenizer.from_pretrained(src, use_fast=True)
 def completions(params: CompletionParams):
     uri = params.text_document.uri
     doc = server.workspace.get_document(uri)
-    pos = params.position
+
+    line = params.position.line
+    char = params.position.character
 
     prompt = ""
 
     if ".chat" in uri:
         prompt = doc.source
     else:
-        prefix = doc.source[:params.position.character]
-        suffix = doc.source[params.position.character:]
-        #prefix = ''.join(doc.lines[:params.position.line])
-        #suffix = ''.join(doc.lines[params.position.line:])
+        prefix = ''.join(doc.lines[:line])
+        prefix = prefix + doc.lines[line][:char]
 
-        #prompt = f"<PRE> {prefix} <SUF> {suffix} <MID>"
-        prompt = prefix + "<FILL_ME>" + suffix
+        suffix = ''.join(doc.lines[line+1:])
+        suffix = doc.lines[line][char:] + suffix
+
+        # This model does not use CodeLlamaTokenizer so manually prompt infill
+        prompt = f"<PRE> {prefix} <SUF> {suffix} <MID>"
+        logging.info(prompt)
 
     server.show_message(message="Infilling...")
 
     input_ids = tokenizer(prompt, return_tensors='pt').input_ids.cuda()
     output = model.generate(
         inputs=input_ids,
-        temperature=0.2,
+        temperature=0.3,
         do_sample=True,
         top_p=0.9,
         top_k=40,
-        max_new_tokens=512,
-        repetition_penalty=1.3,
+        max_new_tokens=256,
+        repetition_penalty=1.1,
     )
-    #out = tokenizer.decode(output[0])
+
     out = tokenizer.batch_decode(output[:, input_ids.shape[1]:], skip_special_tokens = True)[0]
-    #out = out[out.find("<MID>")+5 : out.find("<EOT>")]
+    out = out[:out.find("<EOT>")]
+
     logging.info(out)
 
     lines = out.splitlines()
 
     item = CompletionItem(
             label = "llama",
-            #insert_text = out,
-            #insert_text_mode = InsertTextMode.AsIs,
-            text_edit = TextEdit(
-                range = Range(start=pos, end=pos),
-                new_text = out,
-            ),
+            insert_text = out,
+            insert_text_mode = InsertTextMode.AsIs,
     )
 
     return CompletionList(is_incomplete=False, items=[item])
